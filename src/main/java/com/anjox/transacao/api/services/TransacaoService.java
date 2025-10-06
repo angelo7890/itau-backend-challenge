67e50a58e5114f4e4f30373232a14a8c216806fc
angelo7890 <ar293978@gmail.com>
2025-10-06 01:34:14 -0300

    foi criado uma exception personalizada para o retorno de erro unprocessable entity

--- src/main/java/com/anjox/transacao/api/services/TransacaoService.java
+++ src/main/java/com/anjox/transacao/api/services/TransacaoService.java
@@ -1,6 +1,7 @@
 package com.anjox.transacao.api.services;
 
 import com.anjox.transacao.api.dto.request.RequestTransacaoDto;
+import com.anjox.transacao.api.exceptions.UnprocessableEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -16,10 +17,10 @@ public class TransacaoService {
     public void addTransacao(RequestTransacaoDto dto) {
 
         if(dto.dataHora().isAfter(OffsetDateTime.now())){
-            throw new RuntimeException("data e hora nao pode ser no futuro");
+            throw new UnprocessableEntity("data e hora invalidos");
         }
         if (dto.valor().compareTo(BigDecimal.ZERO)< 0) {
-            throw new RuntimeException("valor nao pode ser negativo");
+            throw new UnprocessableEntity("valor invalido");
         }
         transacoes.add(dto);
     }
