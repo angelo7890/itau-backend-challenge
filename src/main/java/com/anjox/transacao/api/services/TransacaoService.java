772eebe4e3df584871ebc488398f630b5acd8d85
angelo7890 <ar293978@gmail.com>
2025-10-06 01:28:26 -0300

    foi feita a funcao de adicionar uma transacao verificando se a data esta correta e se o valor esta correto, tambem foi feita uma funcao para remover todos os elementos da lista

--- src/main/java/com/anjox/transacao/api/services/TransacaoService.java
+++ src/main/java/com/anjox/transacao/api/services/TransacaoService.java
@@ -3,6 +3,8 @@ package com.anjox.transacao.api.services;
 import com.anjox.transacao.api.dto.request.RequestTransacaoDto;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,4 +12,19 @@ import java.util.List;
 public class TransacaoService {
 
     private final List<RequestTransacaoDto> transacoes = new ArrayList<>();
+
+    public void addTransacao(RequestTransacaoDto dto) {
+
+        if(dto.dataHora().isAfter(OffsetDateTime.now())){
+            throw new RuntimeException("data e hora nao pode ser no futuro");
+        }
+        if (dto.valor().compareTo(BigDecimal.ZERO)< 0) {
+            throw new RuntimeException("valor nao pode ser negativo");
+        }
+        transacoes.add(dto);
+    }
+
+    public void deleteTransacao() {
+        transacoes.clear();
+    }
 }
