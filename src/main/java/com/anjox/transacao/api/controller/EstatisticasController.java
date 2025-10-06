7208f3e06f5a75e01ce150d00e14af2a380da64a
angelo7890 <ar293978@gmail.com>
2025-10-06 02:14:21 -0300

    foi criado as controllers de transacao e estatisticas

---
+++ src/main/java/com/anjox/transacao/api/controller/EstatisticasController.java
@@ -0,0 +1,9 @@
+﻿package com.anjox.transacao.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatisticas")
+public class EstatisticasController {
+}
--- src/main/java/com/anjox/transacao/api/dto/request/RequestTransacaoDto.java
+++ src/main/java/com/anjox/transacao/api/dto/request/RequestTransacaoDto.java
@@ -1,7 +1,14 @@
 ﻿package com.anjox.transacao.api.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-public record RequestTransacaoDto(Double valor, OffsetDateTime dataHora) {
+public record RequestTransacaoDto(
+        @NotNull
+        Double valor,
+        @NotNull
+        OffsetDateTime dataHora
+) {
 }
