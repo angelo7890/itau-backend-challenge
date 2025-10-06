cb96890ed84209362d8622251306a45204219be1
angelo7890 <ar293978@gmail.com>
2025-10-06 02:20:32 -0300

    foi feita as funcoes de adicionar e deletar na controller de transacao

--- src/main/java/com/anjox/transacao/api/controller/TransacaoController.java
+++ src/main/java/com/anjox/transacao/api/controller/TransacaoController.java
@@ -1,9 +1,33 @@
 ﻿package com.anjox.transacao.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.anjox.transacao.api.dto.request.RequestTransacaoDto;
+import com.anjox.transacao.api.services.TransacaoService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
 public class TransacaoController {
+
+    private static final Logger log = LoggerFactory.getLogger(TransacaoController.class);
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTransacao(@RequestBody @Valid RequestTransacaoDto dto){
+        transacaoService.addTransacao(dto);
+        return ResponseEntity.created(null).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteTransacao(){
+        transacaoService.deleteTransacao();
+        return ResponseEntity.ok().build();
+    }
 }
