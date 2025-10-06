63beb23ab428e02779b3f404e6d67b732f5c88b8
angelo7890 <ar293978@gmail.com>
2025-10-06 01:06:32 -0300

    first commit

---
+++ src/test/java/com/anjox/transacao/api/TransacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjox.transacao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
